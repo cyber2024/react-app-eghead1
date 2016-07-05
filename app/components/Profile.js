b1d24b467e7d9627a752dc91f904eaec71cdef4e
cyber2024 <russell.elfenbein@gmail.com>
2016-07-06 05:32:24 +0800

    tutorial complete

---
+++ app/components/Profile.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import Repos from './github/Repos';
+import UserProfile from './github/UserProfile';
+import Notes from './notes/Notes';
+import getGithubInfo from '../utils/helpers';
+import Rebase from 're-base';
+
+const base = Rebase.createClass('https://github-note-taker.firebaseio.com/');
+
+class Profile extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      notes:[],
+      bio: {},
+      repos: []
+    }
+  }
+  handleAddNote(newNote){
+    base.post(this.props.params.username, {
+      data: this.state.notes.concat([newNote])
+    })
+  }
+  componentDidMount(){
+
+    this.initData(this.props.params.username);
+  }
+  componentWillUnmount(){
+    base.removeBinding(this.ref);
+  }
+  componentWillReceiveProps(nextProps){
+    base.removeBinding(this.ref);
+    this.initData(nextProps.params.username);
+  }
+  initData(username){
+    this.ref = base.bindToState(username, {
+      context: this,
+      asArray: true,
+      state: 'notes'
+    });
+
+    getGithubInfo(username)
+      .then(function(data){
+        this.setState({
+          bio: data.bio,
+          repos:data.repos
+        })
+      }.bind(this));
+  }
+  render(){
+    return (
+      <div className="row">
+        <div className="col-md-4">
+          <UserProfile username={this.props.params.username} bio={this.state.bio}/>
+        </div>
+        <div className="col-md-4">
+          <Repos username={this.props.params.username} repos={this.state.repos}/>
+        </div>
+        <div className="col-md-4">
+        <Notes username={this.props.params.username}
+          notes={this.state.notes}
+          addNote={(newNote)=>this.handleAddNote(newNote)}/>
+        </div>
+      </div>
+
+    );
+  }
+}
+
+export default Profile;
